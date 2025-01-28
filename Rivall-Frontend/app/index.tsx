178b07fdd7266bbaad0c0f29f444f6b8d36d54e5
Nathaniel J Reeves <80066234+Nathaniel-Reeves@users.noreply.github.com>
2025-01-27 22:45:27 -0700

    Product details screen linking from tutorial

--- Rivall-Frontend/app/index.tsx
+++ Rivall-Frontend/app/index.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-import products from '../assets/products.json';
+import products from '@/assets/products.json';
 
-import ProductListItem from '../components/ProductListItem';
+import ProductListItem from '@/components/ProductListItem';
 
 export default function HomeScreen() {
   return (
